ef54f1d3213dabb5206341bcbceed755d95d91f1
Resham <reshamupadhyay48@gmail.com>
2025-01-21 22:40:15 +0530

    added signin endpoint

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 const app = express();
 
+app.use(express.json());
 const users = [];
 
 app.post("/signup", function(req, res){
@@ -17,3 +19,29 @@ app.post("/signup", function(req, res){
         msg:"You have signed up"
     })
 })
+
+app.post("/signin", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const foundUser = users.find(user => user.username === username && user.password === password);
+
+    if(foundUser){
+        const token = jwt.sign({
+            username : username
+        },process.env.JWT_SECRETE);
+
+        res.json({
+            token : token
+        })                
+    } else {
+        res.status(403).send({
+            message: "Invalid username or password"
+        })
+    }
+    console.log(users)
+})
+
+
+
+app.listen(3000);
